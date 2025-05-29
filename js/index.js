fe3b09f83c3c20e7c463b55bd1b781bd2c0b4a28
Deep6890 <deepkayastha6890@gmail.com>
2025-05-30 01:04:25 +0530

    Most of the front page first page is deone

---
+++ js/index.js
@@ -0,0 +1,6 @@
+function on_scroll_call_library_animista()
+{
+    var header_element = document.querySelector('header');
+    header_element.style.display = 'flex';
+    console.log('Header is displayed');
+}
