9b5d0445a8a99e123683fa593ce8a8eb593f893a
Deep6890 <deepkayastha6890@gmail.com>
2025-06-11 23:38:42 +0530

    All Portion Done if require some correction may be done. But this is the final looks Js And Responsive website can be mad

--- js/index.js
+++ js/index.js
@@ -20,3 +20,13 @@ function startToggle(className, intervalTime) {
 startToggle("toggle", 1000);        // 1 second
 startToggle("toggle_delay", 200);  // 2 seconds
 startToggle("toggle_delay_3", 3000);
+
+
+// Ad Animista's .bounce-in-bck in rise at the first of my name
+function call_bounce_in_bck() {
+    var main_grid_pointer = document.getElementById(main_grid_my_name);
+    void element.offsetWidth; // Trigger reflow to reset animation
+    ma.classList.add("bounce-in-bck"); // Re-add animation
+    console.log(element);
+}
+
